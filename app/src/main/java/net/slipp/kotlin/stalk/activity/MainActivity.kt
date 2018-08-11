52d73e1260bf7fc6ca9c291c584db41df359fe5b
RudiaMoon <elel3551@naver.com>
2018-08-11 09:32:03 +0900

    프로젝트 세팅환경 구성

---
+++ app/src/main/java/net/slipp/kotlin/stalk/activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package net.slipp.kotlin.stalk.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import net.slipp.kotlin.stalk.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
